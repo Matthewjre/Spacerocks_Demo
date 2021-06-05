9c958e37e022177ce70862282d8964b9586bfaeb
Matthewjre <matthewjre@yahoo.com>
2021-06-05 14:30:25 -0400

    0.5

--- HUD.gd
+++ HUD.gd
@@ -5,3 +5,15 @@ signal start_game
 onready var lives_counter = [$MarginContainer/HBoxContainer/LivesCounter/L1,
 							$MarginContainer/HBoxContainer/LivesCounter/L2,
 							$MarginContainer/HBoxContainer/LivesCounter/L3]
+
+func show_message(message):
+	$MessageLabel.text = message
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func update_score(value):
+	$MarginContainer/HBoxContainer/ScoreLabel.text = str(value)
+
+func update_lives(value):
+	for i in range(3)
+		lives_counter[i].visible = value > i
